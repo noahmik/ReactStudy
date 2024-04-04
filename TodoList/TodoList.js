c4b35e5cfc2506bffca8feeb731fce1ec8a54989
noahmik <s46422574@gmail.com>
2024-04-04 11:49:07 +0900

    ADD,DELETE 버튼 구현

--- TodoList/TodoList.js
+++ TodoList/TodoList.js
@@ -1,22 +1,40 @@
 const list = document.getElementById("list");
 const inputTag = document.getElementById("input");
-const TodoList = document.querySelector('.list');
+const TodoList = document.querySelector(".list");
 
-function TodoListInput () {
+function TodoListInput() {
+  if (window.event.keyCode === 13 && inputTag.value !== '') {
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    const newBtn = document.createElement("button");
 
-    if(window.event.keyCode === 13){
-        const newLi = document.createElement('li');
-		const newSpan = document.createElement('span');
-		const newBtn = document.createElement('button');
-      
-		newLi.appendChild(newBtn); // li안에 button 담기
-		newLi.appendChild(newSpan); // li안에 span 담기
-        newSpan.textContent = inputTag.value;
+    newLi.appendChild(newBtn); // li안에 button 담기
+    newLi.appendChild(newSpan); // li안에 span 담기
+    newSpan.textContent = inputTag.value;
 
-        TodoList.appendChild(newLi);
+    TodoList.appendChild(newLi);
+    inputTag.value = "";
+  }
+}
 
-        inputTag.value = '';
+function TodoListEnter(){
+    if(inputTag.value !== ''){
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    const newBtn = document.createElement("button");
 
-        
-    } 
+    newLi.appendChild(newBtn); // li안에 button 담기
+    newLi.appendChild(newSpan); // li안에 span 담기
+    newSpan.textContent = inputTag.value;
+
+    TodoList.appendChild(newLi);
+    inputTag.value = "";
+    }
+}
+
+function TodoListDelete() {
+    while (TodoList.firstChild) {
+        TodoList.removeChild(TodoList.firstChild);
+        setTimeout(1000)
+    }
 }
